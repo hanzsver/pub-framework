9794babdc218dabd71992595e280af567a99800d
hanzsver <hanzsver@gmail.com>
2024-04-19 11:12:32 +0900

    readme 수정, reset 스타일 추가

--- README.md
+++ README.md
@@ -1,22 +1,53 @@
-# 퍼블리싱 프레임워크
-> 빠른 화면 제작을 위한 퍼블리싱 템플릿 코드 저장소
-> * 반복되는 템플릿을 쉽게 재사용하도록 도와줍니다.
-> * 프로젝트 사용 시 최적화는 필수입니다.
+# Pub Framework
+Pub Framework는 빠른 웹서비스 구축을 위한 시작의 기반입니다.<br/>
+UI 가이드라인과 샘플을 포함하며 모든 서비스 개발에서의 일관된 코드 작성을 도와줍니다.
 
-## 구성 요소
+## Components
 * Layout
-* GNB
-* LNB
 * MGMT
 * CRUD
 * UI Components 
-* ...Others
+
+## Conentions
+* 링크 
+
+## Contributer
+<table>
+	<tr>
+		<td align="center">
+			<a href="https://github.com/hanzsver">
+				<img src="https://avatars.githubusercontent.com/u/146055547?v=4" width="100px;" alt=""/>
+				<br />
+				<b>지은</b>
+			</a>
+		</td>
+		<td align="center">
+			<a href="https://github.com/taetaeil">
+				<img src="https://avatars.githubusercontent.com/u/106569134?v=4" width="100px;" alt=""/>
+				<br />
+				<b>태일</b>
+			</a>
+		</td>
+		<td align="center">
+			<a href="https://github.com/jun0108">
+				<img src="https://avatars.githubusercontent.com/u/96496496?v=4" width="100px;" alt=""/>
+				<br />
+				<b>준영</b>
+			</a>
+		</td>
+		<td align="center">
+			<a href="https://github.com/cro3u">
+				<img src="https://avatars.githubusercontent.com/u/148185835?v=4" width="100px;" alt=""/>
+				<br />
+				<b>다연</b>
+			</a>
+		</td>
+    </tr>
+</table>
 
 ## Setup
 ```
 npm install
-```
-
-```
 npm run serve
 ```
+
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,26 +1,22 @@
 <template>
   <div>
     <index-navbar />
-    <section
-      class="header relative pt-16 items-center flex h-screen max-h-860-px"
-    >
+    <section class="header relative pt-16 items-center flex h-screen max-h-860-px">
       <div class="container mx-auto items-center flex flex-wrap">
         <div class="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
           <div class="pt-32 sm:pt-0">
-            <h2 class="font-semibold text-4xl text-blueGray-600">
-              Pub Framework
-            </h2>
+            <h2 class="font-semibold text-4xl text-blueGray-600">Pub Framework</h2>
             <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
-             Pub Framework는 빠른 웹서비스 구축을 위한 시작의 기반입니다. UI 가이드라인과 샘플을 포함하며 모든 서비스 개발에서의 일관된 코드 작성을 도와줍니다.
+              Pub Framework는 빠른 웹서비스 구축을 위한 시작의 기반입니다. UI 가이드라인과 샘플을 포함하며 모든 서비스
+              개발에서의 일관된 코드 작성을 도와줍니다.
             </p>
             <div class="mt-12">
-              <a
-                href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus?ref=vn-index"
-                target="_blank"
+              <router-link
+                to="/admin/dashboard"
                 class="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-emerald-500 active:bg-emerald-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
               >
                 Get started
-              </a>
+              </router-link>
               <a
                 href="https://github.com/hanzsver/pub-framework"
                 class="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
@@ -37,20 +33,20 @@
   </div>
 </template>
 <script>
-import IndexNavbar from "@/components/Navbars/IndexNavbar.vue";
-import FooterComponent from "@/components/Footers/Footer.vue";
+import IndexNavbar from '@/components/Navbars/IndexNavbar.vue';
+import FooterComponent from '@/components/Footers/Footer.vue';
 
-import patternVue from "@/assets/img/pattern_vue.png";
-import componentBtn from "@/assets/img/component-btn.png";
-import componentProfileCard from "@/assets/img/component-profile-card.png";
-import componentInfoCard from "@/assets/img/component-info-card.png";
-import componentInfo2 from "@/assets/img/component-info-2.png";
-import componentMenu from "@/assets/img/component-menu.png";
-import componentBtnPink from "@/assets/img/component-btn-pink.png";
-import documentation from "@/assets/img/documentation.png";
-import login from "@/assets/img/login.jpg";
-import profile from "@/assets/img/profile.jpg";
-import landing from "@/assets/img/landing.jpg";
+import patternVue from '@/assets/img/pattern_vue.png';
+import componentBtn from '@/assets/img/component-btn.png';
+import componentProfileCard from '@/assets/img/component-profile-card.png';
+import componentInfoCard from '@/assets/img/component-info-card.png';
+import componentInfo2 from '@/assets/img/component-info-2.png';
+import componentMenu from '@/assets/img/component-menu.png';
+import componentBtnPink from '@/assets/img/component-btn-pink.png';
+import documentation from '@/assets/img/documentation.png';
+import login from '@/assets/img/login.jpg';
+import profile from '@/assets/img/profile.jpg';
+import landing from '@/assets/img/landing.jpg';
 
 export default {
   data() {
