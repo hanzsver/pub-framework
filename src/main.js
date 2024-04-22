823e9845ef762ab55d2b5bc5e701a2b80f29cb52
hanzsver <hanzsver@gmail.com>
2024-04-22 14:33:50 +0900

    feat: tailwind css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 // styles
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@/assets/styles/tailwind.css';
 import '@/assets/styles/main.scss';
 
 // mouting point for the whole app
